66f072176b724e4d461f44619eb758b8f61e0a02
Electro Games <2cool4u567@gmail.com>
2021-12-04 18:54:31 -0600

    Add files via upload

---
+++ src/Electro/AntiCombatLog/AntiCombatLog.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Electro\AntiCombatLog;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\event\Listener;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class AntiCombatLog extends PluginBase implements Listener{
+
+    public array $playersInCombat = [];
+    public array $bannedCommands = [];
+    public string $bannedCommandMsg;
+    public string $enteredCombatMsg;
+    public string $exitCombatMsg;
+    public float $combatTime;
+    public bool $quitKill;
+
+    public function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        foreach ($this->getConfig()->get("BannedCommands") as $cmd)
+        {
+            $this->bannedCommands[] = $cmd;
+        }
+        $this->bannedCommandMsg = $this->getConfig()->get("BannedCommandMsg");
+        $this->enteredCombatMsg = $this->getConfig()->get("EnterCombatMsg");
+        $this->exitCombatMsg = $this->getConfig()->get("ExitCombatMsg");
+        $this->combatTime = $this->getConfig()->get("CombatTime");
+        $this->quitKill = $this->getConfig()->get("QuitKill");
+        $this->combatTask();
+    }
+
+    public function onDamage(EntityDamageByEntityEvent $event)
+    {
+        $player = $event->getEntity();
+        $damager = $event->getDamager();
+
+        if ($event->isCancelled()) return;
+        if (!$player instanceof Player || !$damager instanceof Player) return;
+        if ($player->isCreative() || $damager->isCreative()) return;
+
+        foreach ([$player, $damager] as $player)
+        {
+            if (!isset($this->playersInCombat[$player->getName()]))
+            {
+                $player->sendMessage($this->enteredCombatMsg);
+            }
+            $this->playersInCombat[$player->getName()] = $this->combatTime;
+        }
+    }
+
+    public function onChat(PlayerCommandPreprocessEvent $event)
+    {
+        $player = $event->getPlayer();
+        $msg = $event->getMessage();
+        if (!isset($this->playersInCombat[$player->getName()])) return;
+        $msg = substr($msg, 1);
+        $msg = explode(" ", $msg);
+        if(!in_array($msg[0], $this->bannedCommands)) return;
+        $event->cancel();
+        $player->sendMessage($this->bannedCommandMsg);
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
+
+        if (!isset($this->playersInCombat[$player->getName()])) return;
+        if (!$this->quitKill) return;
+        $player->kill();
+    }
+
+    public function combatTask()
+    {
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(
+            function() {
+                foreach ($this->playersInCombat as $playerName => $time)
+                {
+                    $time--;
+                    $this->playersInCombat[$playerName]--;
+                    if ($time <= 0)
+                    {
+                        unset($this->playersInCombat[$playerName]);
+                        $player = $this->getServer()->getPlayerExact($playerName);
+                        if ($player instanceof Player)
+                        {
+                            $player->sendMessage($this->exitCombatMsg);
+                        }
+                    }
+                }
+            }
+        ), 20);
+    }
+
+}
